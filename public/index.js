edebd037a8b9f660362eaf486fd7866a3d314028
mario <mario@Awakening.local>
2017-09-05 01:00:57 +0900

    ejsライブラリの導入(htmlを利用できるようにするため)

--- public/index.js
+++ public/index.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
-app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-	  response.send('Hello World!')
+	  response.render('index', {});
 });
 
 app.listen(app.get('port'), function() {
