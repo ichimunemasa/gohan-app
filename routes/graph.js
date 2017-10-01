f609e8362a98f4948ec476f615b46e36abd643b9
ichimunemasa <isshu.mumanesa@gmail.com>
2017-10-01 11:41:13 +0900

    グラフの種類, 軸の変更

--- routes/graph.js
+++ routes/graph.js
@@ -11,20 +11,20 @@ exports.showGraph = function(socket){
     model.query('SELECT * FROM member', function(err, rows) {
       var dataPlotMale = [
         { label: "れい",  y: rows[0]["num_of_votes_male"],indexLabel:String(rows[0]["num_of_votes_male"])},
-        { label: "いっちー",y: rows[1]["num_of_votes_male"],indexLabel:String(rows[1]["num_of_votes_male"]) },
+        { label: "いちのき",y: rows[1]["num_of_votes_male"],indexLabel:String(rows[1]["num_of_votes_male"]) },
         { label: "ももか",y: rows[2]["num_of_votes_male"],indexLabel:String(rows[2]["num_of_votes_male"]) },
         { label: "ひるた",y: rows[3]["num_of_votes_male"],indexLabel:String(rows[3]["num_of_votes_male"])},
         { label: "ぼみ", y: rows[4]["num_of_votes_male"],indexLabel:String(rows[4]["num_of_votes_male"]) },
-        { label: "いっしゅー", y: rows[5]["num_of_votes_male"],indexLabel:String(rows[5]["num_of_votes_male"])}
+        { label: "いっしゅう", y: rows[5]["num_of_votes_male"],indexLabel:String(rows[5]["num_of_votes_male"])}
       ];
 
       var dataPlotFemale = [
         { label: "れい",  y: rows[0]["num_of_votes_female"], indexLabel:String(rows[0]["num_of_votes_female"])},
-        { label: "いっちー",y: rows[1]["num_of_votes_female"],indexLabel:String(rows[1]["num_of_votes_female"]) },
+        { label: "いちのき",y: rows[1]["num_of_votes_female"],indexLabel:String(rows[1]["num_of_votes_female"]) },
         { label: "ももか",y: rows[2]["num_of_votes_female"],indexLabel:String(rows[2]["num_of_votes_female"]) },
         { label: "ひるた",y: rows[3]["num_of_votes_female"], indexLabel:String(rows[3]["num_of_votes_female"])},
         { label: "ぼみ", y: rows[4]["num_of_votes_female"],indexLabel:String(rows[4]["num_of_votes_female"]) },
-        { label: "いっしゅー", y: rows[5]["num_of_votes_female"],indexLabel:String(rows[5]["num_of_votes_female"])}
+        { label: "いっしゅう", y: rows[5]["num_of_votes_female"],indexLabel:String(rows[5]["num_of_votes_female"])}
       ];
       //console.log(dataPlotFemale)
       //console.log(dataPlotMale)
